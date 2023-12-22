c15539d4b61da3b2a3115049c76f48a5bc2d7386
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-22 16:25:58 +0600

    api created for tasks

--- index.js
+++ index.js
@@ -29,12 +29,68 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('technovisionDB').collection('users');
+    const taskCollection = client.db('technovisionDB').collection('tasks');
+
+
+
+// user 
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email }
+    const existUser = await userCollection.findOne(query);
+    if (existUser) {
+      return res.send({ message: 'user exist' })
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result)
+  })
+
+
+//   tasks 
+
+app.post('/tasks',async(req,res)=>{
+    const task = req.body;
+    const result = await taskCollection.insertOne(task)
+    res.send(result)
+  })
+
+  app.get('/tasks',async(req,res)=>{
+    const email = req.query?.email
+    const result = await taskCollection.find({email:email}).toArray()
+    res.send(result)
+  })
+
+  app.put('/tasks/:id',async(req,res)=>{
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)}
+    const options = { upsert: true };
+    const status = req.body.status;
+    const updatedDoc = {
+      $set:{
+        status: status
+      }
+    }
+    const result = await taskCollection.updateOne(filter,updatedDoc,options)
+    res.send(result)
+  })
+
+  app.delete('/tasks/:id',async(req,res)=>{
+    const id = req.params.id;
+    const filter = {_id : new ObjectId(id)}
+    const result = await taskCollection.deleteOne(filter)
+    res.send(result)
+  })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
